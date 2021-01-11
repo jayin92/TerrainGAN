a297ece04fd5d1ff87ae78c33bb922cae23ee11f
jayin92 <jayin920805@gmail.com>
2021-01-11 13:42:12 +0000

    0110

---
+++ data/sathei_dataset.py
@@ -0,0 +1,44 @@
+import os.path
+from data.base_dataset import BaseDataset, get_params, get_transform
+from data.image_folder import make_dataset
+from PIL import Image
+
+
+class SatHeiDataset(BaseDataset):
+    
+    def __init__(self, opt):
+        
+        BaseDataset.__init__(self, opt)
+        self.dir = os.path.join(opt.dataroot, opt.phase)  # get the image directory
+        self.paths = sorted(make_dataset(self.dir, opt.max_dataset_size))  # get image paths
+        assert(self.opt.load_size >= self.opt.crop_size)   # crop_size should be smaller than the size of loaded image
+        self.input_nc =  self.opt.input_nc
+        self.output_nc =  self.opt.output_nc
+
+    def __getitem__(self, index):
+
+        # read a image given a random integer index
+        path = self.paths[index]
+        ABC = Image.open(path).convert('RGB')
+        # split AB image into A and B
+        w, h = ABC.size
+        w2 = int(w / 3)
+        A = ABC.crop((0, 0, w2, h))
+        B = ABC.crop((w2, 0, w2*2, h))
+        C = ABC.crop((w2*2, 0, w2*3, h))
+
+        # apply the same transform to both A and B
+        transform_params = get_params(self.opt, A.size)
+        A_transform = get_transform(self.opt, transform_params, grayscale=1)
+        B_transform = get_transform(self.opt, transform_params, grayscale=0)
+        C_transform = get_transform(self.opt, transform_params, grayscale=1)
+
+        A = A_transform(A)
+        B = B_transform(B)
+        C=C_transform(C)
+
+        return {'A': A, 'B': B,'C':C, 'A_paths': path, 'B_paths': path, 'C_paths': path}
+
+    def __len__(self):
+        """Return the total number of images in the dataset."""
+        return len(self.paths)
--- models/vae_pix2pix2_model.py
+++ models/vae_pix2pix2_model.py
@@ -1,6 +1,7 @@
 """
 空照圖和高度圖一起train
 輸入是模糊圖，輸出是空照和清晰圖
+SatHeiDataset
 """
 import torch
 from .base_model import BaseModel
@@ -8,7 +9,7 @@ from . import networks
 import random
 import numpy as np
 
-class VAEPix2PixModel2(BaseModel):
+class VAEPix2Pix2Model(BaseModel):
     """ This class implements the pix2pix model, for learning a mapping from input images to output images given paired data.
 
     The model training requires '--dataset_mode aligned' dataset.
@@ -34,16 +35,18 @@ class VAEPix2PixModel2(BaseModel):
         By default, we use vanilla GAN loss, UNet with batchnorm, and aligned datasets.
         """
         # changing the default values to match the pix2pix paper (https://phillipi.github.io/pix2pix/)
-        parser.set_defaults(norm='batch', netG='unet_256', dataset_mode='aligned')
+        print('test+++++++++++++++++++++-------------========================================')
+        parser.set_defaults(pool_size=0, gan_mode='vanilla',dataset_mode='sathei')
         if is_train:
-            parser.set_defaults(pool_size=0, gan_mode='vanilla')
+            
             parser.add_argument('--lambda_L1', type=float, default=100.0, help='weight for L1 loss')
 
         return parser
 
     def __init__(self, opt):
+        
         """Initialize the pix2pix class.
-
+        
         Parameters:
             opt (Option class)-- stores all the experiment flags; needs to be a subclass of BaseOptions
         """
@@ -51,25 +54,27 @@ class VAEPix2PixModel2(BaseModel):
         # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
         self.loss_names = ['G_GAN', 'G_L1', 'D_real', 'D_fake','KLD','miu','var']
         # specify the images you want to save/display. The training/test scripts will call <BaseModel.get_current_visuals>
-        self.visual_names = ['real_A', 'fake_B', 'real_B']
+        self.visual_names = ['real_A', 'real_B', 'real_C', 'fake_B', 'fake_C']
         # specify the models you want to save to the disk. The training/test scripts will call <BaseModel.save_networks> and <BaseModel.load_networks>
         if self.isTrain:
             self.model_names = ['S','G', 'D']
         else:  # during test time, only load G
             self.model_names = ['S','G']
         # define networks (both generator and discriminator)
-        latent_dim = 8
-        self.beta = 0.001
-        self.epoch=0# used to schedule beta 
+        latent_dim = 4
+        self.beta = 0.008
+        self.sat_weight = 1
+        self.epoch=0 # used to schedule beta 
         
         
-        self.netS = networks.MyDataParallel(networks.VAEEncoder2(opt.input_nc+opt.output_nc,latent_dim,[32,64,128,256,256,256,8,8]),self.gpu_ids)if opt.useVAE2 else networks.MyDataParallel(networks.VAEEncoder(opt.input_nc+opt.output_nc,latent_dim,[32,64,128,256,256,256,8,8]),self.gpu_ids)
+        self.netS = networks.MyDataParallel(networks.VAEEncoder2(5,latent_dim,[32,64,128,256,256,256,8,8]),self.gpu_ids)if opt.useVAE2 else networks.MyDataParallel(networks.VAEEncoder(5,latent_dim,[32,64,128,256,256,256,8,8]),self.gpu_ids)
         
-        self.netG = networks.define_G(opt.input_nc+latent_dim, opt.output_nc, opt.ngf, opt.netG, opt.norm,not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids,att=opt.attention,multsc=opt.mult_skip_conn)
-                                  
+        self.netG = networks.define_G(1+latent_dim, 4, opt.ngf, opt.netG, opt.norm,not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids,att=opt.attention,multsc=opt.mult_skip_conn)
+        
+        self.tanh=torch.nn.Tanh()
 
         if self.isTrain:  # define a discriminator; conditional GANs need to take both input and output images; Therefore, #channels for D is input_nc + output_nc
-            self.netD = networks.define_D(opt.input_nc + opt.output_nc + latent_dim, opt.ndf, opt.netD,
+            self.netD = networks.define_D(5 + latent_dim, opt.ndf, opt.netD,
                                           opt.n_layers_D, opt.norm, opt.init_type, opt.init_gain, self.gpu_ids)
 
         if self.isTrain:
@@ -84,58 +89,61 @@ class VAEPix2PixModel2(BaseModel):
             self.optimizers.append(self.optimizer_S)
             self.optimizers.append(self.optimizer_G)
             self.optimizers.append(self.optimizer_D)
-            self.epoch=0
+            self.epoch = 0
     
     def set_input(self, input,epoch=0):
-        """Unpack input data from the dataloader and perform necessary pre-processing steps.
-
-        Parameters:
-            input (dict): include the data itself and its metadata information.
-
-        The option 'direction' can be used to swap images in domain A and domain B.
-        """
         self.epoch=epoch
-        AtoB = self.opt.direction == 'AtoB'
-        self.real_A = input['A' if AtoB else 'B'].to(self.device)
-        self.real_B = input['B' if AtoB else 'A'].to(self.device)
-        self.image_paths = input['A_paths' if AtoB else 'B_paths']
+        self.real_A = input['A'].to(self.device) #blurry
+        self.real_B = input['B'].to(self.device) #sat
+        self.real_C = input['C'].to(self.device) #hei
+        self.image_paths = input['A_paths']
         if self.opt.isTrain:    
             if random.randint(0,1):
                 self.real_A=torch.flip(self.real_A,[2])
                 self.real_B=torch.flip(self.real_B,[2])
+                self.real_C=torch.flip(self.real_C,[2])
             if random.randint(0,1):
                 self.real_A=torch.flip(self.real_A,[3])
                 self.real_B=torch.flip(self.real_B,[3])
+                self.real_C=torch.flip(self.real_C,[3])
             if random.randint(0,1):
                 self.real_A=self.real_A.transpose(2,3)
                 self.real_B=self.real_B.transpose(2,3)    
+                self.real_C=self.real_C.transpose(2,3) 
     def test(self):
-        self.visual_names = ['real_A', 'fake_B', 'real_B','rand_lat']
+        self.visual_names = ['real_A', 'real_B', 'real_C', 'fake_B', 'fake_C','rand_lat_B','rand_lat_C']
         self.netS.eval()
         self.netG.eval()
-        self.style = self.netS(torch.cat([self.real_A, self.real_B],dim=1)) 
-        self.fake_B = self.netG(torch.cat([self.real_A, self.style],dim=1))
-
+        self.style = self.netS(torch.cat([self.real_A, self.real_B, self.real_C],dim=1))
+        fake_BC = self.netG(torch.cat([self.real_A, self.style],dim=1))
+        self.fake_B=self.tanh(fake_BC[:,0:3])
+        self.fake_C=fake_BC[:,3:4]
+        
         self.style = torch.randn_like(self.style[:,:,0:1,0:1]).repeat(1,1,256,256)
-        self.rand_lat = self.netG(torch.cat([self.real_A, self.style],dim=1))
+        fake_BC = self.netG(torch.cat([self.real_A, self.style],dim=1))
+        self.rand_lat_B=self.tanh(fake_BC[:,0:3])
+        self.rand_lat_C=fake_BC[:,3:4]
             
             
     def forward(self, latent=None):
         rand_bias=np.random.uniform(-2,2)
         """Run forward pass; called by both functions <optimize_parameters> and <test>."""
         if latent == None:
-            self.style = self.netS(torch.cat([self.real_A+rand_bias if self.netG.module.output_nc==3 else self.real_A, self.real_B],dim=1)) #+ torch.tensor((np.random.rand(1, 16, 1, 1) * 2 - 1) / 10, dtype=torch.float).to("cuda:0")
+            self.style = self.netS(torch.cat([self.real_A+rand_bias, self.real_B, self.real_C+rand_bias],dim=1))
         else:
             self.style = latent
-        self.fake_B = self.netG(torch.cat([self.real_A+rand_bias if self.netG.module.output_nc==3 else self.real_A, self.style],dim=1))#random bias
+        fake_BC = self.netG(torch.cat([self.real_A+rand_bias, self.style],dim=1))
+        self.fake_B=self.tanh(fake_BC[:,0:3])
+        self.fake_C=fake_BC[:,3:4]-rand_bias
+        
     def backward_D(self):
         """Calculate GAN loss for the discriminator"""
         # Fake; stop backprop to the generator by detaching fake_B
-        fake_AB = torch.cat([self.real_A, self.fake_B,self.style], dim=1)  # we use conditional GANs; we need to feed both input and output to the discriminator
+        fake_AB = torch.cat([self.real_A, self.fake_B,self.fake_C,self.style], dim=1)  # we use conditional GANs; we need to feed both input and output to the discriminator
         pred_fake = self.netD(fake_AB.detach())
         self.loss_D_fake = self.criterionGAN(pred_fake, False)
         # Real
-        real_AB = torch.cat([self.real_A, self.real_B,self.style.detach()], dim=1)
+        real_AB = torch.cat([self.real_A, self.real_B,self.real_C,self.style.detach()], dim=1)
         pred_real = self.netD(real_AB)
         self.loss_D_real = self.criterionGAN(pred_real, True)
         # combine loss and calculate gradients
@@ -145,12 +153,12 @@ class VAEPix2PixModel2(BaseModel):
     def backward_G(self):
         """Calculate GAN and L1 loss for the generator"""
         # First, G(A) should fake the discriminator
-        fake_AB = torch.cat((self.real_A, self.fake_B,self.style), 1)
+        fake_AB = torch.cat([self.real_A, self.fake_B,self.fake_C,self.style], dim=1)
         pred_fake = self.netD(fake_AB)
         self.loss_G_GAN = self.criterionGAN(pred_fake, True)
         # Second, G(A) = B
-        self.loss_G_L1 = self.criterionL1(self.fake_B, self.real_B) * self.opt.lambda_L1
-        self.loss_KLD = self.netS.loss(self.beta*(0.001+(self.epoch%50)/50.0))
+        self.loss_G_L1 = (self.criterionL1(self.fake_B, self.real_B)*self.sat_weight+self.criterionL1(self.fake_C, self.real_C)) * self.opt.lambda_L1
+        self.loss_KLD = self.netS.loss(self.beta*(0.001+(self.epoch%50)/50.0)) #periodic beta sceduling
         self.loss_miu=self.netS.rmsMiu()
         self.loss_var=self.netS.meanVar()
         # combine loss and calculate gradients
